c5df651b1e9af1e0fb287032d9cc77155d622f59
techmonk05 <chkarthik2328@gmail.com>
2026-06-18 11:39:19 +0530

    test 1

--- public/push-handler.js
+++ public/push-handler.js
@@ -1,13 +1,8 @@
 // public/push-handler.js
 self.addEventListener('push', function(event) {
-  if (!event.data) return;
-  const data = event.data.json();
   event.waitUntil(
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: '/icons/icon-192x192.png',
-      vibrate: [100, 50, 100],
-      data: { url: data.url ?? '/' },
+    self.registration.showNotification('Test', {
+      body: 'Hello from push handler',
     })
   );
 });
