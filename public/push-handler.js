d2447071043d2a95516ee9a2dfcc431f4bd89781
techmonk05 <chkarthik2328@gmail.com>
2026-06-18 11:42:45 +0530

    test 2

--- public/push-handler.js
+++ public/push-handler.js
@@ -1,9 +1,11 @@
 // public/push-handler.js
 self.addEventListener('push', function(event) {
+  console.log('PUSH EVENT RECEIVED', event);
   event.waitUntil(
     self.registration.showNotification('Test', {
       body: 'Hello from push handler',
-    })
+    }).then(() => console.log('Notification shown successfully'))
+      .catch(err => console.error('Notification failed:', err))
   );
 });
 
